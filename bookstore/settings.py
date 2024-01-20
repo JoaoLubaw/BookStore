7cc59f5f732064f5d7d79da1936875e3f0f38cad
João Lubawaski <jlubawaski@gmail.com>
2024-01-20 11:08:27 -0300

    Fixxing

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -88,11 +88,11 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'DB_NAME'),
-        'USER': os.environ.get('DB_USER', 'DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'roundhouse.proxy.rlwy.net'),
-        'PORT': os.environ.get('DB_PORT', 36141),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST' 'roundhouse.proxy.rlwy.net'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
