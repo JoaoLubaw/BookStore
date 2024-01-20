0663148f2053a5527b11337185049ed68cecc8ea
João Lubawaski <jlubawaski@gmail.com>
2024-01-20 10:41:35 -0300

    Fix

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME', 'DB_NAME'),
         'USER': os.environ.get('DB_USER', 'DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'DB_HOST'),
+        'HOST': os.environ.get('DB_HOST', 'roundhouse.proxy.rlwy.net'),
         'PORT': os.environ.get('DB_PORT', 36141),
     }
 }
