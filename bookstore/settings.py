6b119e452103ae4748f5482a7ef9190a013df15c
João Lubawaski <jlubawaski@gmail.com>
2024-01-20 10:35:48 -0300

    DB_FIX

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -88,11 +88,11 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('DB_NAME', 'DB_NAME'),
+        'USER': os.environ.get('DB_USER', 'DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', 'DB_PORT'),
     }
 }
 
